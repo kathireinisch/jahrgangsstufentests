12cd2c1a8afd6ad368df64adb2eafc5c4d1fd288
kathireinisch <katharina.reinisch@mnet-mail.de>
2021-05-16 11:34:54 +0200

    update ex 2

--- src/afterOne/Main.java
+++ src/afterOne/Main.java
@@ -42,6 +42,7 @@ public class Main {
         BufferedReader reader = new BufferedReader(new FileReader(filepath));
         String line;
         String id ="";
+        char[] allowedChars = {'A', 'T', 'C', 'G'};
         while (((line=reader.readLine()))!=null){
             if(line.startsWith(">")){
                 //wichtige annotationen einlesen, gen objekt damit erstellen
@@ -50,7 +51,15 @@ public class Main {
                 String name = attributes[1];
                 geneList.put(id, new Gene(id, "", name));
             } else {
+                String test ="helloAT";
                 //alles was nach der Gene id zeile kommt, gehoert zur sequenz des jeweiligen gens
+                //falls die sequenz ein zeichen != A/T/C/G enthält, wird das gen rausgelassen
+                if (!line.matches("[A|T|C|G]*")){
+                    if (geneList.get(id)!= null){
+                        geneList.remove(id);
+                    }
+                    continue;
+                }
                 String seq = geneList.get(id).getSeq();
                 seq=seq+line;
                 geneList.get(id).setSeq(seq);
