2db60a1216e05752468ca21701448d785307e551
Your Name <you@example.com>
2021-05-18 12:13:19 +0200

    Aufgabe 3

--- src/afterOne/Gene.java
+++ src/afterOne/Gene.java
@@ -1,3 +1,4 @@
+
 public class Gene {
     private String name;
     private String id;
@@ -32,4 +33,104 @@ public class Gene {
     public String getId() {
         return id;
     }
+
+
+
+
+    //Aufgabe 3
+    //3a)
+    public int getLength(){
+        int length = seq.length();
+        return length; //or return seq.length();
+    }
+
+    //3b)
+    public float calculateGcContent() {
+
+        //initilaize counter for G/C-Content in String
+        int gcCounter = 0;
+
+        //iterare through every char in String
+        for (int i = 0; i < seq.length(); i++) {
+            char base = seq.charAt(i);
+
+            //check if char C or G: if yes -> increase counter
+            if(base == 'C' || base == 'G'){
+                gcCounter++;
+            }
+        }
+
+        //get length of Sequence by calling getLength() (from 3a)
+        int length = getLength();
+
+        //calculate GC-Content: gcCounter divided by length
+        //use float, since division could give you decimal digits
+        float gcContent = gcCounter/length;
+
+        //return gcContent, since calculateGcContent() is not void (= doesn't return anything), but float
+        return gcContent; //or return (gcCounter/length);
+    }
+
+    //3c)
+    public String calculateComplement(){
+        //initialize new String, where the complement will be build
+        String complementString = "";
+
+
+        //iterare through every char in String
+        for (int i = 0; i < seq.length(); i++) {
+            char base = seq.charAt(i);
+
+            //1. Option: if-statements
+            //check base and append complement of this base to new String (complementString)
+            if(base == 'C'){
+                complementString += 'G';
+            }
+            if(base == 'G'){
+                complementString += 'C';
+            }
+            if(base == 'A'){
+                complementString += 'T';
+            }
+            if(base == 'T'){
+                complementString += 'A';
+            }
+
+            //2. Option: switch-case
+            switch (base){
+                case 'G':
+                    complementString += 'C';
+                    break;
+                case 'C':
+                    complementString += 'G';
+                    break;
+                case 'A':
+                    complementString += 'T';
+                    break;
+                case 'T':
+                    complementString += 'A';
+                    break;
+            }
+
+        }
+
+        return complementString;
+    }
+
+
+    //3d)
+    public String calculateReverseComplement(){
+        //calculate Complement by calling calculateComplement()
+        String complement = calculateComplement();
+
+        //reverse this String
+        String reverse = new StringBuffer(complement).reverse().toString();
+        //or other methods from the internet (e.g: https://www.geeksforgeeks.org/reverse-a-string-in-java/)
+
+        return reverse;
+    }
+
+
+
+
 }
--- src/afterOne/Main.java
+++ src/afterOne/Main.java
@@ -6,6 +6,7 @@ import net.sourceforge.argparse4j.inf.Namespace;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Main {
@@ -67,5 +68,57 @@ public class Main {
 
         }
         return geneList;
+    }
+
+
+
+
+    //Aufgabe 3
+    //3e)
+    public void calculateAlignment(Gene gene, Gene otherGene){
+        //get Sequence of both Gene objects
+        String sequence = gene.getSeq();
+        String otherSequence = otherGene.getSeq();
+
+        //initialize alignment String and score counter
+        String alignment = "";
+        int score = 0;
+
+        //iterate through one seuence (does not matter which one since, both have the same length)
+        for (int i = 0; i < sequence.length(); i++) {
+
+            //check if both sequences have the same character at position i
+            //if yes: append this character to alignmentString and increase score by 1
+            //if no: append "N" to alignmentString
+            if(sequence.charAt(i) == otherSequence.charAt(i)){
+                alignment += sequence.charAt(i);
+                score++;
+            }else{
+                alignment += "N";
+            }
+        }
+
+        //print Alignment and Score
+        System.out.println("Alignment: "+alignment);
+        System.out.println("Score: "+ score);
+    }
+
+    //3f)
+    public void printGeneNames(ArrayList<Gene> genes){
+
+        //iterate through every Gene object in ArrayList ("genes")
+        for(Gene gene : genes){
+            //get name of gene by calling getName()
+            String name = gene.getName();
+
+            //print names separated by "," (without going into a new line: System.out.print()) (with new line: System.out.println())
+            //caution: last gene will have "," at the end
+            //(if you don't want this, one of many options is to build a new empty String before the for-loop: String geneNames = "";
+            // iterate through for-loop and append gene names with "," to this String:  geneNames += name+ "," ;
+            // after for-loop eliminate last element of string (","): String newGeneNames = geneNames.substring(0,geneNames.lenght()-2);
+            // print this new String: System.out.println(newGeneNames);
+
+            System.out.print(name+ ","); // or System.out.print(gene.getName()+ ",")
+        }
     }
 }
